d341627418edc6d137ec8896a47e85b12e59134a
Wojtek Kajstura <wojtek.kajstura@gmail.com>
2024-03-08 12:49:19 +0100

    migration

---
+++ server/RaiNbow/Core/Storage/Migrations/20240307215650_Initial - proof of concept.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace RaiNbow.Core.Storage.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initialproofofconcept : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "data");
+
+            migrationBuilder.CreateTable(
+                name: "FieldType",
+                schema: "data",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    CSharpTypeName = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FieldType", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Schemas",
+                schema: "data",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Schemas", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Fields",
+                schema: "data",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    FieldTypeId = table.Column<int>(type: "integer", nullable: false),
+                    SchemaId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Fields", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Fields_FieldType_FieldTypeId",
+                        column: x => x.FieldTypeId,
+                        principalSchema: "data",
+                        principalTable: "FieldType",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Fields_Schemas_SchemaId",
+                        column: x => x.SchemaId,
+                        principalSchema: "data",
+                        principalTable: "Schemas",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Fields_FieldTypeId",
+                schema: "data",
+                table: "Fields",
+                column: "FieldTypeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Fields_SchemaId",
+                schema: "data",
+                table: "Fields",
+                column: "SchemaId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Fields",
+                schema: "data");
+
+            migrationBuilder.DropTable(
+                name: "FieldType",
+                schema: "data");
+
+            migrationBuilder.DropTable(
+                name: "Schemas",
+                schema: "data");
+        }
+    }
+}
---
+++ server/RaiNbow/Core/Storage/Migrations/20240307222125_FieldType.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RaiNbow.Core.Storage.Migrations
+{
+    /// <inheritdoc />
+    public partial class FieldType : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                schema: "data",
+                table: "FieldType",
+                newName: "TypeName");
+
+            migrationBuilder.RenameColumn(
+                name: "CSharpTypeName",
+                schema: "data",
+                table: "FieldType",
+                newName: "FieldName");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "TypeName",
+                schema: "data",
+                table: "FieldType",
+                newName: "Name");
+
+            migrationBuilder.RenameColumn(
+                name: "FieldName",
+                schema: "data",
+                table: "FieldType",
+                newName: "CSharpTypeName");
+        }
+    }
+}
