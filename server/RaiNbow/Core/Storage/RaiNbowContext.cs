d321257d39315dccdecc01f7666db4023df30f26
Wojtek Kajstura <wojtek.kajstura@gmail.com>
2024-03-12 22:12:55 +0100

    WIP: working on PoC of creating tables for schema

--- server/RaiNbow/Core/Storage/RaiNbowContext.cs
+++ server/RaiNbow/Core/Storage/RaiNbowContext.cs
@@ -1,16 +1,19 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 using RaiNbow.Cms;
 
 namespace RaiNbow.Core.Storage;
 
 public class RaiNbowContext : IdentityDbContext<IdentityUser>
 {
+    private readonly ILogger<RaiNbowContext> _logger;
     public DbSet<Schema> Schemas { get; set; }
     public DbSet<Field> Fields { get; set; }
-    public RaiNbowContext(DbContextOptions<RaiNbowContext> options) : base(options)
+    public RaiNbowContext(DbContextOptions<RaiNbowContext> options, ILogger<RaiNbowContext> logger) : base(options)
     {
+        _logger = logger;
     }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
@@ -18,5 +21,30 @@ public class RaiNbowContext : IdentityDbContext<IdentityUser>
         base.OnModelCreating(modelBuilder);
         
         modelBuilder.HasDefaultSchema(StorageConstants.DataSchema);
+    }
+
+    public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken())
+    {
+        var addedEntities = ChangeTracker.Entries()
+            .Where(e => e.State == EntityState.Added)
+            .Select(e => e.Entity)
+            .ToList();
+
+        foreach (var entity in addedEntities)
+        {
+            if (entity is Schema schema)
+            {
+                await OnCreateSchema(schema);
+            }
+        }
+        
+        return await base.SaveChangesAsync(cancellationToken);
+    }
+
+    // TODO:
+    private Task OnCreateSchema(Schema schema)
+    {
+        _logger.LogInformation($"{nameof(Schema)} {schema.Name} is created. Creating table for this schema...");
+        return Task.CompletedTask;
     }
 }
