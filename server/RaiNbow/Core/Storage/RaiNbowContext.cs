058c01cbddfd148b86a6121a58192255d4e23402
Wojtek Kajstura <wojtek.kajstura@gmail.com>
2024-03-14 20:19:36 +0100

    WIP: Creating tables for each schema

--- server/RaiNbow/Core/Storage/RaiNbowContext.cs
+++ server/RaiNbow/Core/Storage/RaiNbowContext.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
@@ -41,11 +42,38 @@ public class RaiNbowContext : IdentityDbContext<IdentityUser>
         return await base.SaveChangesAsync(cancellationToken);
     }
 
-    // TODO:
-    private Task OnCreateSchema(Schema schema)
+    private async Task OnCreateSchema(Schema schema)
     {
-        _logger.LogInformation($"{nameof(Schema)} {schema.Name} is created. Creating table for this schema...");
+        _logger.LogInformation($"{nameof(Schema)} {schema.Name} is created. Creating table for this schema.");
         
-        return Task.CompletedTask;
+        var createTableSql = $"CREATE TABLE \"{schema.Name}\" (Id SERIAL PRIMARY KEY)";
+
+        try
+        {
+            await Database.ExecuteSqlRawAsync(createTableSql);
+            _logger.LogInformation($"Table '{schema.Name}' has been created successfully.");
+
+            await AddMigration($"CreateTable_{schema.Name}");
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError($"Failed to create table '{schema.Name}': {ex.Message}");
+            throw; // Rethrow the exception to indicate failure
+        }
+    }
+
+    // TODO: Check how it can be done via code
+    private async Task AddMigration(string migrationName)
+    {
+        _logger.LogInformation($"Creating migration: {migrationName}.");
+
+        // var migrationProcess = Process.Start(
+        //     "dotnet", 
+        //     $@"ef migrations add {migrationName} 
+        //                     --project RaiNbow 
+        //                     --startup-project RaiNbow.API 
+        //                     --output-dir Core/Storage/Migrations");
+        //
+        // await migrationProcess.WaitForExitAsync();
     }
 }
