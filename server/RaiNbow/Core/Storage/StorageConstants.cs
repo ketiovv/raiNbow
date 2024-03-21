426e7a7ffe7adb8c98db052d10874e3418c25a41
Wojtek Kajstura <wojtek.kajstura@gmail.com>
2024-03-21 21:32:17 +0100

    added id field name to StorageConstants.cs

--- server/RaiNbow/Core/Storage/StorageConstants.cs
+++ server/RaiNbow/Core/Storage/StorageConstants.cs
@@ -5,4 +5,5 @@ public static class StorageConstants
     public const string CmsSchema = "cms";    
     public const string DataSchema = "data";    
     public const string CmsTablePrefix = "rainbow";
+    public const string IdFieldName = "Id";
 }
