1912f7d9a0b0cee442ee64aa690d64aeaece95a2
Wojtek Kajstura <wojtek.kajstura@gmail.com>
2024-03-21 21:33:48 +0100

    registered GenericSchemaRepository in the IoC

--- server/RaiNbow/Cms/Configuration.cs
+++ server/RaiNbow/Cms/Configuration.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.DependencyInjection;
 using RaiNbow.Cms.CreateSchema;
 using RaiNbow.Cms.GetAllSchemas;
 
@@ -6,6 +7,13 @@ namespace RaiNbow.Cms;
 
 internal static class Configuration
 {
+    internal static IServiceCollection AddCmsServices(this IServiceCollection services)
+    {
+        services.AddScoped<IGenericSchemaRepository, GenericSchemaRepository>();
+        
+        return services;
+    }
+
     internal static IEndpointRouteBuilder UseCmsEndpoints(this IEndpointRouteBuilder endpoints)
         => endpoints
             .UseCreateSchemaEndpoint()
